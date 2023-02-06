3c612229a5100fde0c95e20557b9bd2ec5e24389
trokenlox <123604824+trokenlox@users.noreply.github.com>
2023-02-07 02:54:24 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./ProductList/ProductList";
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
@@ -16,6 +17,9 @@ function App() {
   return (
     <div className="App">
         <Header />
+        <Routes>
+            <Route index element={<ProductList/>}/>
+        </Routes>
     </div>
   );
 }
